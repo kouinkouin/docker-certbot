00a28e95320e41435bdbae45d7c9ca584ee3e8d5
kouinkouin <me@kouinkouin.com>
2018-02-08 15:43:13 +0100

    fix(certbot): cli.ini is now optional

--- files/run.sh
+++ files/run.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
-[ "$EMAIL" != "" ] && sed -i -r "s/^\#?email\ =.*$/email = ${EMAIL}/" /etc/letsencrypt/cli.ini
-exec $@
+CONF_INI=/etc/letsencrypt.ini
+
+if [ ! -f $CONF_INI ]; then
+    echo "$CONF_INI unfindable, that's not a standard situation"
+    exec $@
+else
+    [ "$EMAIL" != "" ] && sed -i -r "s/^\#?email\ =.*$/email = ${EMAIL}/" $CONF_INI
+
+    if [ "$1" == 'certbot' -a -f $CONF_INI ]; then
+        shift
+        exec certbot -i $CONF_INI --agree-tos $@
+    else
+        exec $@
+    fi
+fi
 
